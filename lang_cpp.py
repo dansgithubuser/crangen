3c7e2793422fc70c120b3e919876306d3aff3253
Dan <dansonlinepresence@gmail.com>
2016-02-03 01:40:46 -0500

    fix lang_cpp gen_enum

--- lang_cpp.py
+++ lang_cpp.py
@@ -2,7 +2,7 @@ import style
 
 def gen_enum(name, series):
 	result='enum class {0}{{\n'.format(name)
-	for i in series: result+='\t{0},\n'.format(style.to_upper(style.from_camel(i)))
+	for i in series: result+='\t{0},\n'.format(style.to_upper(i))
 	result+='\tSENTINEL\n'
 	result+='};\n'
 	return result
